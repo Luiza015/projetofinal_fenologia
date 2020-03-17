b9318becc2738399b6f87104f0554898cf794675
Luiza <luizambastos@hotmail.com>
2020-03-17 11:09:20 -0300

    YAML atualizado do relatorio markdown

--- R/projeto_final.R
+++ R/projeto_final.R
@@ -1,6 +1,7 @@
 ##Trabalho final Projeto de análise de dados em R##
 #Fenologia de Pteris congesta#
 
+###_________________________________###Parte I
 #criando objeto com a pasta
 
 files.path <- list.files (path = "data",
@@ -20,7 +21,7 @@ mean(comp_media$comp_folha_jovem)
 head (comp_media)
 summary(comp_media)
 
-#Qual a média das variáveis por espécie? Usando as funções agreggate e tapply
+#Qual a média das variáveis por mês? Usando as funções agreggate e tapply
 
 #media do comprimento de folha fértil por meses
 tapply(X = comp_media$comp_folha_fertil, INDEX = list(comp_media$meses), FUN = mean)
@@ -69,7 +70,7 @@ lines(density(comp_media$comp_folha_jovem), col="blue") #note que agora estamos 
 boxplot(comp_folha_fertil ~ meses, data = comp_media)
 
 
-####________________________________________####
+####________________________________________####Parte II
 files.path2  <- list.files (path = "data",
                           pattern = ".csv",
                           full.names = TRUE)
@@ -77,14 +78,50 @@ files.path2
 precip_temp <- read.csv2(files.path[2])
 precip_temp
 
+
+#Para salvar figuras
+png("Figs/figura01.png", res = 300, width = 2400, height = 1200)
 hist(precip_temp$preciptacao_mm)
 plot(precip_temp$preciptacao_mm)
 title("Gráficos preciptação ao longo dos meses 2019")
+dev.off()
 
+png("Figs/figura02.png", res = 300, width = 2400, height = 1200)
 plot(precip_temp$T_max_media)
 title("Gráfico temp máxima 2019")
+dev.off()
 
+png("Figs/figura03.png", res = 300, width = 2400, height = 1200)
 plot(precip_temp$T_min_media)
 title("Gráfico temp mínima 2019")
+dev.off()
 
 
+###_______________________________________###Parte III
+files.path3 <- list.files (path = "data/analise_circular",
+                           pattern = ".csv",
+                           full.names = TRUE)
+files.path3
+analise_circular <- read.csv2(files.path3)
+analise_circular
+#Para verificar se o arquivo foi lido corretamente
+str(analise_circular)
+
+#Istalando o pacote "circular" para que as análises sejam feitas
+library(circular)
+
+#A planilha deve ter uma coluna para os ângulos e colunas para os comprimentos
+borda <- rep (analise_circular$ângulo, analise_circular$comp_folha_fertil)
+interior <- rep (analise_circular$ângulo, analise_circular$comp_folha_esteril)
+
+#Transformando os dados em radiano
+borda.rad <- rad(borda)
+interior.rad <- rad (interior)
+#Transformando os dados em circulares
+borda.circ <- as.circular(borda.rad)
+interior.circ <- as.circular(interior.rad)
+
+#Dessa maneira, foi criado dois vetores, cujos valores dos ângulos se repetem. Pode-se dessa maneira, começar a visualizar os dados. Para isso, colocar os dados no gráfico:
+
+plot.circular(cbind(borda.circ, interior.circ), rotation = "clock", units = "rads")
+rose.diag(cbind(interior.circ, borda.circ))
--- README.md
+++ README.md
@@ -3,3 +3,19 @@ Objetivos:
   Gerar médias, desvios padrões e gráficos paraa examinar se existe relação entre a dinâmica das fenofases vegetativas e reprodutivas com variação das características abióticas (preciptação e temperatura).
 Hipótese: preciptação e temperatura influenciam positivamente na fertilidade e natalidade.
 Resultados esperados: em períodos com maior quantidade de chuva, a tendência é de ter mais folhas férteis assim como o surgimento de folhas novas e báculos. Nos períodos mais secos a probabilidade de mortalidade foliar é maior. 
+
+Passos do script:
+#Parte I e II
+1- Criando objeto a partir da pasta.
+2- Fazendo média de cada coluna da tabela.
+3- Aplicando as funções head() e summary() para inspecionar todos os arquivos; head (cabeçalho); summary (média, mediana, etc).
+4- Média das variáveis por mês usando as funções agreggate e tapply.
+5- Fazendo curva de densidade- mostra a probabilidade de observar determinado valor. Em comparação ao histograma, no eixo y, ao invés de termos a frequência, temos a densidade probabilística.
+6- Plot das curvas de densidade sobre o histograma.
+7- Ao final da execucação de todos os gráficos, eles devem ser salvos como figura a partir da função: png("Figs/figura02.png", res = 300, width = 2400, height = 1200)
+dados de como o gráfico foi feito
+dev.off()
+
+
+#Parte III
+Essa parte foi um teste usando o pacote "circular" para tentar gerar dados iniciais com a análise circular, decidi deixar as informações, mesmo incompletas, para posteriomente com os dados completos de fenologia continuar os gráficos. 
